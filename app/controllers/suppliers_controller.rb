a47f42add49983565c39dc4cf9163e6e021632ea
lexiht <hazukitran@gmail.com>
2017-07-05 21:31:07 +0100

    Option to create supplier

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,25 @@
+class SuppliersController < ApplicationController
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    supplier = Supplier.new(supplier_params)
+    if supplier.save
+      flash[:notice] = 'You have successfully added a supplier!'
+      redirect_to dashboard_url
+    else
+      flash[:alert] = ''
+      supplier.errors.each do |key, value|
+        flash[:alert] += "#{key}: #{value}"
+      end
+      redirect_to new_supplier_path
+    end
+  end
+
+  private
+
+  def supplier_params
+    params.require(:supplier).permit(:name, :email, :website, :phone)
+  end
+end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,4 @@
 class Supplier < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
 end
