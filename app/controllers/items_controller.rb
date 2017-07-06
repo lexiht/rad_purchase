6a6f334aae3663bb069653431ba4091a9ae8d988
lexiht <hazukitran@gmail.com>
2017-07-06 15:40:58 +0100

    Add new item to supplier

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,25 @@ class ItemsController < ApplicationController
     @supplier = Supplier.find(params[:supplier_id])
     @item = @supplier.items.find(params[:id])
   end
+
+  def create
+    @supplier = Supplier.find(params[:supplier_id])
+    @item = @supplier.items.new(item_params)
+    if @item.save
+      flash[:notice] = 'You have successfully added an item!'
+      redirect_to supplier_path(@supplier)
+    else
+      flash[:alert] = ''
+      supplier.errors.each do |key, value|
+        flash[:alert] += "#{key}: #{value}"
+      end
+      redirect_to new_supplier_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :product_code, :description, :quantity, :price)
+  end
 end
